53e92951e378a8864be911b2d9dd312441c293e8
inseo24 <84627144+inseo24@users.noreply.github.com>
2024-05-01 09:00:12 +0900

    leetcode 49

---
+++ leetcode/string/49_group_anagram.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/group-anagrams/description/
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> m = new HashMap<>();
+
+        for (String str: strs) {
+            String sorted = str.chars().sorted().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining());
+            m.computeIfAbsent(sorted, k -> new ArrayList<>()).add(str);
+        }
+
+        List<List<String>> answer = new ArrayList<>();
+        for (String key: m.keySet()) {
+            answer.add(m.get(key));
+        }
+
+        return answer;
+    }
+}
