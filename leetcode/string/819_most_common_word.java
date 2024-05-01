0d3c4bf22f12c8ea6a86650e51199c80f9256049
inseo24 <84627144+inseo24@users.noreply.github.com>
2024-05-01 23:41:23 +0900

    2트 추가

--- leetcode/string/819_most_common_word.java
+++ leetcode/string/819_most_common_word.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// 1트
 // https://leetcode.com/problems/most-common-word/description/
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
@@ -37,5 +38,25 @@ class Solution {
         }
 
         return answer;
+    }
+}
+
+
+// ㅋㅋ 2트
+import java.util.*;
+
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Map<String, Integer> freq = new HashMap<>();
+        String[] a = paragraph.replaceAll("[!?',;.]", " ").toLowerCase().split(" ");
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        
+        for (String word: a) {
+            if (word.length() > 0 && !ban.contains(word)) {
+                freq.put(word, freq.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        return Collections.max(freq.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
