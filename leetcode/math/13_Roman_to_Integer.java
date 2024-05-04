2fde097354f8a606af3b1a3c8d02eb60100f6e0a
inseo24 <84627144+inseo24@users.noreply.github.com>
2024-05-04 14:47:58 +0900

    추가

--- leetcode/math/13_Roman_to_Integer.java
+++ leetcode/math/13_Roman_to_Integer.java
@@ -17,3 +17,77 @@ class Solution {
         return answer;
     }
 }
+
+// 걍 냅다 쓰는 코드가 성능은 가장 잘 나왔다는,, 
+class Solution {
+    public static int romanToInt(String s) {
+        int res = 0;
+        char[] let = s.toCharArray();
+        for(int i = 0; i < let.length; i++) { //IV
+            switch (let[i]) {
+                case 'I':
+                    if (i + 1 < let.length) {
+                        if (let[i + 1] == 'V') {
+                            res += 4;
+                            i++;
+                            break;
+                        } else if (let[i + 1] == 'X') {
+                            res += 9;
+                            i++;
+                            break;
+                        }
+                        res++;
+                        break;
+                    }
+                    res++;
+                    break;
+                case 'V':
+                    res += 5;
+                    break;
+                case 'X':
+                    if (i + 1 < let.length) {
+                        if (let[i + 1] == 'L') {
+                            res += 40;
+                            i++;
+                            break;
+                        } else if (let[i + 1] == 'C') {
+                            res += 90;
+                            i++;
+                            break;
+                        }
+                        res += 10;
+                        break;
+                    }
+                    res += 10;
+                    break;
+                case 'L':
+                    res += 50;
+                    break;
+                case 'C':
+                    if (i + 1 < let.length) {
+                        if (let[i + 1] == 'D') {
+                            res += 400;
+                            i++;
+                            break;
+                        } else if (let[i + 1] == 'M') {
+                            res += 900;
+                            i++;
+                            break;
+                        }
+                        res += 100;
+                        break;
+                    }
+                    res += 100;
+                    break;
+                case 'D':
+                    res += 500;
+                    break;
+                case 'M':
+                    res += 1000;
+                    break;
+            }
+        }
+
+        return res;
+    }
+}
